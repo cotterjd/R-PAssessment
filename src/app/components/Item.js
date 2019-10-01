8262bde5e83953251645927557380d919fc1d63e
Jordan Cotter <jrdn.cttr@gmail.com>
2019-10-01 15:03:27 -0500

    front end. 1st iteration

--- src/app/components/Item.js
+++ src/app/components/Item.js
@@ -1,15 +1,15 @@
 import React from 'react'
-
+import {ItemContainer, LinkImg} from './styled'
 export function Item({data}) {
 		return (
-			<div style={{display: 'grid', gridTemplate: '1fr 1fr 1fr 1fr 3fr 1fr 1fr'}}>
-        <span>{data.badge}</span>
+			<ItemContainer>
+        <img width="50px" src={require('../../assets/images/placeholder.png')} alt="launch icon" />
         <span>{data.rocketName}</span>
         <span>{data.rocketType}</span>
         <span>{data.launchDate}</span>
         <span>{data.details}</span>
         <span>{data.id}</span>
-        <span>{data.article}</span>
-			</div>
+        <LinkImg width="25px" src={require('../../assets/images/link.svg')} alt="external link icon"/>
+			</ItemContainer>
 		)
 }
--- src/app/components/styled/index.js
+++ src/app/components/styled/index.js
@@ -1,5 +1,9 @@
 import styled from 'styled-components'
 
+const dataGrid = `
+  display: grid;
+  grid-template: 1fr / 80px 120px 120px 120px 500px 40px 80px;
+`
 export const CheckboxLabel = styled.label`
     text-transform: uppercase;
     color: #fff;
@@ -9,9 +13,15 @@ export const ColumnHeader = styled.span`
   `
 export const ColumnHeaders = styled.div`
     background: linear-gradient(#a3bbd0, #fff);
-    display: grid;
-    grid-template: 1fr / 1fr 1fr 1fr 1fr 3fr 1fr 1fr;
     padding: 20px;
+    ${dataGrid}
+  `
+export const ItemContainer = styled.div`
+    ${dataGrid}
+    background-color: #dbdfe5;
+    border-radius: 5px;
+    height: 50px;
+    align-items: center;
   `
 export const Filters = styled.div`
     display: flex;
@@ -24,6 +34,9 @@ export const Filters = styled.div`
       display: flex;
     }
   `
+export const LinkImg = styled.img`
+    justify-self: center;
+  `
 export const RefreshIcon = styled.img`
     border: 1px solid #fff;
     border-radius: 50%;
@@ -31,6 +44,8 @@ export const RefreshIcon = styled.img`
   `
 export const TableBody = styled.div`
     display: grid;
+    grid-gap: 5px;
+    margin-top: 5px;
   `
 export const TableHeader = styled.div`
     display: grid;
